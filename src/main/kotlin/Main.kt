689a30d618dc52301c76a679398e963f68089965
Maksim <maksimiakovenko123321@gmail.com>
2025-02-05 16:18:00 +0100

    UN-1-3 do smth

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,5 +2,11 @@ package org.example
 
 fun main() {
     val lesson1 = lesson1_task1()
+    val lesson2 = lesson1_task2()
+    val lesson3 = lesson1_task3()
+
+
     lesson1.print()
+    lesson2.print1()
+    lesson3.print2()
 }
---
+++ src/main/kotlin/lesson1_task3.kt
@@ -0,0 +1,7 @@
+package org.example
+
+class lesson1_task3 {
+    fun print2 () {
+        println("lesson 1_3")
+    }
+}
