aa7b338a111285fb570cd6085d0be2f0023dd8d5
Maksim <maksimiakovenko123321@gmail.com>
2025-02-05 15:53:38 +0100

    UN-1-1 refactor smth

--- src/main/kotlin/lesson1_task1.kt
+++ src/main/kotlin/lesson1_task1.kt
@@ -2,10 +2,7 @@ package org.example
 
 class lesson1_task1 {
     fun print() {
-        var i = 0
-        i ++
         println("Hello World!")
         println("10")
-        println("${i}")
     }
 }
